e13f586ede634dc8819ee1d9eb85940e3ac2bd82
Nguyen Tri Trung <tritrung232@gmail.com>
2020-09-03 16:16:52 +0700

    add folder

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Nhap_Xuat
+{
+    class Nhap_Xuat
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("gia tri:" + Console.ReadLine()); // đọc dữ liệu từ bàn phím cho đến khi gặp ký tự xuống dòng thì dừng. Sau đó in giá trị đã nhập ra màn hình.
+            Console.WriteLine("gia tri: "+ Console.ReadLine());
+            Console.ReadKey(true);// lệnh này dùng với mục đích dừng màn hình để xem kết quả., true thì không in ra kí tự nhập
+        }
+
+    }
+}
